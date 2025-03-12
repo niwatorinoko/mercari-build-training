acfddba03a7c908da6b60a99b89890396432418e
niwatorinoko <rean.ogswr@gmail.com>
2025-03-13 00:37:01 +0800

    Docker ComposeでAPIとフロントエンドを動かす

--- typescript/simple-mercari-web/src/components/ItemList.tsx
+++ typescript/simple-mercari-web/src/components/ItemList.tsx
@@ -29,20 +29,30 @@ export const ItemList = ({ reload, onLoadCompleted }: Prop) => {
   }, [reload, onLoadCompleted]);
 
   return (
-    <div>
+    <div className="ItemList">
       {items?.map((item) => {
+        const imageUrl = item.image_name
+          ? `http://localhost:9000/images/${item.image_name}`
+          : PLACEHOLDER_IMAGE;
+
         return (
-          <div key={item.id} className="ItemList">
-            {/* TODO: Task 2: Show item images */}
-            <img src={PLACEHOLDER_IMAGE} />
-            <p>
-              <span>Name: {item.name}</span>
+          <p key={item.id} className="ItemCard">
+            <img 
+              src={imageUrl} 
+              alt={item.name} 
+              className="ItemImage"
+              onError={(e) => {
+                e.currentTarget.src = PLACEHOLDER_IMAGE;
+              }} 
+            />
+            <p className="ItemInfo">
+              <span className="ItemName">{item.name}</span>
               <br />
-              <span>Category: {item.category}</span>
+              <span className='ItemCategory'>Category: {item.category_name}</span>
             </p>
-          </div>
+          </p>
         );
       })}
     </div>
-  );
+  );  
 };
